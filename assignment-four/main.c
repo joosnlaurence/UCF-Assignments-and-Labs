bd0f653a8b090be5c7b7d8d6a6c2565362541944
joosnlaurence <jasongotanl@gmail.com>
2024-10-22 23:51:42 -0400

    Update main

--- assignment-four/main.c
+++ assignment-four/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#define CLOSER -1
-#define FARTHER 1
+#define CLOSER -999
+#define FARTHER 999
 
 typedef struct coords{
     int x, y;
@@ -9,17 +10,34 @@ typedef struct coords{
 
 coords personCoords;
 
-coords* ReadData(){
-
+// Return the distance between the point and the user's coordinates
+float calcDist(int x, int y){
+    return sqrt(pow(x - personCoords.x, 2) + pow(y - personCoords.y, 2));
 }
+
+coords* ReadData(int* numG, int* s, int* t){
+    scanf("%d %d", &personCoords.x, &personCoords.y);
+
+    scanf("%d %d %d", numG, s, t);
+
+    coords* c = (coords*)malloc(sizeof(coords) * (*numG));
+    for(int i = 0; i < *numG; i++){
+        scanf("%d %d", &c[i].x, &c[i].y);
+    }
+
+    return c;
+}
+/* Return -999 if point 1 is closer to the user than point 2
+   Return 999 otherwise 
+*/
 int compareTo(coords* ptrPt1, coords* ptrPt2){
     int x1 = ptrPt1->x, x2 = ptrPt2->x;
     int y1 = ptrPt1->y, y2 = ptrPt2->y;
     if(x1 == x2 && y1 == y2) // if same points
         return 0;
 
-    float distPt1 = sqrt(pow(x1 - personCoords.x, 2) + pow(y1 - personCoords.y, 2));
-    float distPt2 = sqrt(pow(x2 - personCoords.x, 2) + pow(y2 - personCoords.y, 2));
+    float distPt1 = calcDist(x1, y1);
+    float distPt2 = calcDist(x2, y2);
 
     if(distPt1 < distPt2)
         return CLOSER;
@@ -33,26 +51,41 @@ int compareTo(coords* ptrPt1, coords* ptrPt2){
     }
 }
 
-void binSearch();
+int bin_search(coords* gArr, int n, int px, int py){
+    int l = 0, r = n;
+    int val = calcDist(px, py);
+    while(l <= r){
+        int mid = (r+l)/2;
+        int x = gArr[mid].x, y = gArr[mid].y;
+        float dist = calcDist(x, y);
+        if(val < dist)
+            r = mid - 1;
+        else if(val > dist)
+            l = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+} 
 
 void merge();
-void mergesort(coords* coords, int l, int r, int t);
+void merge_sort(coords* coords, int l, int r, int t);
 
 void insertion_sort(coords* coords, int l, int r);
 
 void wrapper(coords* gCoords, int n, int threshold);
 
+void print_coords(coords* coords, int n){
+    for(int i = 0; i < n; i++){
+        printf("%d %d\n", coords->x, coords->y);
+    }
+}
+
 int main(){
-    coords c1;
-    c1.x = 1;
-    c1.y = 0;
-    coords c2;
-    c2.x = 0;
-    c2.y = 1;
-    personCoords.x = 10;
-    personCoords.y = 10;
+    int numGarages, numPoints, threshold;
+    coords* c = ReadData(&numGarages, &numPoints, &threshold);
+    //wrapper(c, numGarages, threshold);
 
-    printf("%d\n", compareTo(&c1, &c2));
-
+    print_coords(c, numGarages);
     return 0;
 }
