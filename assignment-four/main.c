45a9ffb1a3e6b2dee1e041ca4ad5835a9dc864b4
joosnlaurence <jasongotanl@gmail.com>
2024-10-23 02:38:02 -0400

    Create sorting and search algs and handle queries

--- assignment-four/main.c
+++ assignment-four/main.c
@@ -6,36 +6,45 @@
 
 typedef struct coords{
     int x, y;
+    float distToPerson;
 }coords;
 
 coords personCoords;
 
-/* Return the distance between the point and the user's coordinates */
-float calcDist(int x, int y){
-    return sqrt(pow(x - personCoords.x, 2) + pow(y - personCoords.y, 2));
-}
+/* Return the distance between the point and the user's coordinates 
+   Was the idea I used for most of the time, so I'll keep it here*/
+// float calcDist(int x, int y){
+//     return sqrt(pow(x - personCoords.x, 2) + pow(y - personCoords.y, 2));
+// }
 
 /* Slightly optimized calculation for distance that removes the need for math.h
-    Comparing the squared euclidean distances is enough for sorting
+    Comparing the squared euclidean distances is enough for searching and sorting
 */
-// float calcDist(int x, int y){
-//     return (x - personCoords.x)*(x - personCoords.x) + 
-//            (y - personCoords.y)*(y - personCoords.y);
-// }
+float calcDist(int x, int y){
+    return (x - personCoords.x)*(x - personCoords.x) + 
+           (y - personCoords.y)*(y - personCoords.y);
+}
+
+/* Read input and initialize the values for s (number of queries), t (threshold), 
+   numG (number of Garages), and the coords for the array and person
+*/
 coords* ReadData(int* numG, int* s, int* t){
     scanf("%d %d", &personCoords.x, &personCoords.y);
-
+    personCoords.distToPerson = 0;
     scanf("%d %d %d", numG, s, t);
 
     coords* c = (coords*)malloc(sizeof(coords) * (*numG));
     for(int i = 0; i < *numG; i++){
         scanf("%d %d", &c[i].x, &c[i].y);
+        c[i].distToPerson = calcDist(c[i].x, c[i].y);
     }
 
     return c;
 }
-/* Return -999 if point 1 is closer to the user than point 2
-   Return 999 otherwise 
+
+/* Return -999 (CLOSER) if first point is closer to the user than point 2
+   Return 999 (FARTHER) if first point is farther than point 2
+   Return 0 if the two points are the same 
 */
 int compareTo(coords* ptrPt1, coords* ptrPt2){
     int x1 = ptrPt1->x, x2 = ptrPt2->x;
@@ -43,14 +52,15 @@ int compareTo(coords* ptrPt1, coords* ptrPt2){
     if(x1 == x2 && y1 == y2) // if same points
         return 0;
 
-    float distPt1 = calcDist(x1, y1);
-    float distPt2 = calcDist(x2, y2);
+    float distPt1 = ptrPt1->distToPerson;
+    float distPt2 = ptrPt2->distToPerson;
 
     if(distPt1 < distPt2)
         return CLOSER;
     else if(distPt1 > distPt2)
         return FARTHER;
     else{ // same distance from person but distinct points
+    // Smaller x or y coordinate is considered closer
         if(x1 != x2) 
             return x1 < x2 ? CLOSER : FARTHER;
         else
@@ -58,16 +68,17 @@ int compareTo(coords* ptrPt1, coords* ptrPt2){
     }
 }
 
-int bin_search(coords* gArr, int n, int px, int py){
+/* Uses binary search to look for the given coordinates in the coords array */
+int bin_search(coords* garageCoords, int n, coords* val){
     int l = 0, r = n-1;
-    float val = calcDist(px, py);
     while(l <= r){
         int mid = (r+l)/2;
-        int x = gArr[mid].x, y = gArr[mid].y;
-        float dist = calcDist(x, y);
-        if(val < dist)
+        int result = compareTo(val, &garageCoords[mid]);
+        // Val coordinate is closer to person than mid coordinate
+        if(result == CLOSER) 
             r = mid - 1;
-        else if(val > dist)
+        // Val coordinate is farther from person than mid coordinate
+        else if(result == FARTHER)
             l = mid + 1;
         else
             return mid;
@@ -75,10 +86,20 @@ int bin_search(coords* gArr, int n, int px, int py){
     return -1;
 } 
 
+/* Insertion sort used with merge sort to sort the subarrays of the coords array*/
 void insertion_sort(coords* c, int l, int r){
-    
+    for(int i = l+1; i <= r; i++){
+        int k = i;
+        coords currCoord = c[k];
+        while( k > l && compareTo(&currCoord, &c[k-1]) == CLOSER ){
+            c[k] = c[k-1];
+            k--;
+        }
+        c[k] = currCoord;
+    }
 }
 
+/* Merges two sub arrays, using m as the index separating them */
 void merge(coords* c, int l, int m, int r){
     int l_size = m - l + 1;
     int r_size = r - m;
@@ -87,28 +108,32 @@ void merge(coords* c, int l, int m, int r){
     for(int i = 0; i < l_size; i++){
         l_arr[i].x = c[i + l].x;
         l_arr[i].y = c[i + l].y;
+        l_arr[i].distToPerson = c[i + l].distToPerson;
     }
     for(int i = 0; i < r_size; i++){
         r_arr[i].x = c[i + m + 1].x;
         r_arr[i].y = c[i + m + 1].y;
+        r_arr[i].distToPerson = c[i + m + 1].distToPerson;
     }
 
     int i = 0, j = 0;
     int k = l;
     while(i < l_size && j < r_size){
-        float distL = calcDist(l_arr[i].x, l_arr[i].y);
-        float distR = calcDist(r_arr[j].x, r_arr[j].y);
-        if(distL < distR){
+        // If ith point in l_arr is closer to the person than jth point in r_arr,
+        // set the spot at k in the original coords array to the ith point in l_arr
+        if( compareTo(&l_arr[i], &r_arr[j]) == CLOSER ){
             c[k] = l_arr[i];
             i++;
         }
         else{
             c[k] = r_arr[j];
-            j++;
+            j++; 
         }
         k++;
     }
 
+    // Insert the points of whichever subarray hasn't been used up yet 
+    // into the original coords array
     while(i < l_size){
         c[k] = l_arr[i];
         i++;
@@ -122,6 +147,9 @@ void merge(coords* c, int l, int m, int r){
     free(l_arr);
     free(r_arr);
 }
+/* Sorts the coordinates with respect to each's distance to the person in ascending order.
+   If the length of the subarray reaches the threshold t, use insertion sort on the subarray
+*/
 void merge_sort(coords* coords, int l, int r, int t){
     if( (r + 1 - l) <= t ){
         insertion_sort(coords, l, r);
@@ -135,15 +163,39 @@ void merge_sort(coords* coords, int l, int r, int t){
     }
 }
 
-
 /* Wrapper function for sorting the garages by coordinate */
 void sort(coords* gCoords, int n, int threshold){
     merge_sort(gCoords, 0, n-1, threshold);
 }
 
-void print_coords(coords* coords, int n){
+/* Prints coords to console in the format: xi, yi*/
+void print_coords(coords* c, int n){
     for(int i = 0; i < n; i++){
-        printf("%d %d\n", coords->x, coords->y);
+        printf("%d %d\n", c[i].x, c[i].y);
+    }
+}
+
+/* Checks if the numQueries amount of points from the input are in the garage coordinates array
+   If not found, print out the points that weren't found
+   If found, print out the points and where they were found
+ */
+void handle_queries(coords* c, int numGarages, int numQueries){
+    for(int i = 0; i < numQueries; i++){
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        coords temp;
+        temp.x = x;
+        temp.y = y;
+        temp.distToPerson = calcDist(x, y);
+
+        int index = bin_search(c, numGarages, &temp);
+        if(index == -1){
+            printf("%d %d no garage found\n", x, y);
+        }
+        else{
+            printf("%d %d garage found at position %d in the order\n", x, y, index+1);
+        }
     }
 }
 
@@ -153,5 +205,10 @@ int main(){
     sort(c, numGarages, threshold);
 
     print_coords(c, numGarages);
+
+    handle_queries(c, numGarages, numPoints);
+
+    free(c);
+    
     return 0;
 }
