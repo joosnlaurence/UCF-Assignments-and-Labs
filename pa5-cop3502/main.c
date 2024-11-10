0059c1362bbc33b188f1456cf6c0e4a5e8eb3ca8
joosnlaurence <jasongotanl@gmail.com>
2024-11-10 06:26:21 -0500

    Finished deduct function and debugged other files

--- pa5-cop3502/main.c
+++ pa5-cop3502/main.c
@@ -1,3 +1,6 @@
+/* COP3502C: Assignment 5
+    This program was created by Jason Laureano
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,17 +12,10 @@
 typedef struct violation_node{
     char name[MAX_NAME_LEN+1];
     int fine_amt;
-    int depth; 
+    int depth; // Keep track of the depth in the tree for every node
     struct violation_node* left;
     struct violation_node* right;
 }violation_node;
-void print_inorder(violation_node* root){
-    if(root != NULL){
-        print_inorder(root->left);
-        printf("(%s %d %d) ", root->name, root->fine_amt, root->depth);
-        print_inorder(root->right);
-    }
-}
 
 violation_node* createNode(char name[MAX_NAME_LEN+1], int fine){
     violation_node* node = (violation_node*)malloc(sizeof(violation_node));
@@ -30,61 +26,67 @@ violation_node* createNode(char name[MAX_NAME_LEN+1], int fine){
 
     return node;
 }
-int findDepth(violation_node* root, char name[MAX_NAME_LEN+1]){
-    if(root == NULL) return -1;
-    
-    int cmp = strcmp(name, root->name);
-    int depth;
-
-    if(cmp < 0)
-        depth = findDepth(root->left, name);
-    else if(cmp > 0)
-        depth = findDepth(root->right, name);
-    else
-        depth = 0;
-    
-    return (depth == -1) ? -1 : depth + 1;
-}
-violation_node* addHelper(violation_node* root, char name[MAX_NAME_LEN+1], int fine, int depth){
+violation_node* add(violation_node* root, char name[MAX_NAME_LEN+1], int fine, int depth){
     if(root == NULL){
         violation_node* temp = createNode(name, fine);
         temp->depth = depth;
+        printf("%s %d %d\n", name, temp->fine_amt, temp->depth);
         return temp;
     } 
 
     int cmp = strcmp(name, root->name);
     if(cmp < 0)
-        root->left = addHelper(root->left, name, fine, depth+1);
+        root->left = add(root->left, name, fine, depth+1);
     else if(cmp > 0)
-        root->right = addHelper(root->right, name, fine, depth+1);
+        root->right = add(root->right, name, fine, depth+1);
     else{
+        // Add to the fine if the person already has a fine
         root->fine_amt += fine;
+        root->depth = depth;
+        printf("%s %d %d\n", name, root->fine_amt, depth);
     }
 
     return root;
 }
-violation_node* add(violation_node* root, char name[MAX_NAME_LEN+1], int fine){
-    return addHelper(root, name, fine, 0);
-}
 
-violation_node* search(violation_node* root, char name[MAX_NAME_LEN+1]){
+violation_node* search(violation_node* root, char name[MAX_NAME_LEN+1], int depth){
     if(root == NULL) return NULL;
     
     int cmp = strcmp(name, root->name);
-    if(cmp < 0)
-        return search(root->left, name);
-    else if(cmp > 0)
-        return search(root->right, name);
-    else
+    // Update depths upon every search to account for depth changes due to deletion 
+    if(cmp < 0){
+        root->depth = depth;
+        return search(root->left, name, depth+1);
+    }
+    else if(cmp > 0){
+        root->depth = depth;
+        return search(root->right, name, depth+1);
+    }
+    else{
+        root->depth = depth;
         return root;
+    }
 }
-violation_node* parent(violation_node* root, violation_node* node){}
+violation_node* parent(violation_node* root, violation_node* node){
+    if(root == NULL || node == root)
+        return NULL;
+    
+    if(root->left == node || root->right == node)
+        return root;
+    int cmp = strcmp(node->name, root->name);
+    if(cmp < 0)
+        return parent(root->left, node);
+    else if(cmp > 0)
+        return parent(root->right, node);
+    else
+        return NULL;
+}
 violation_node* maxVal(violation_node* root){
     if(root == NULL) return 0;
 
     if(root->right != NULL)
         return maxVal(root->right);
-    return root->fine_amt;
+    return root;
 }
 int isLeaf(violation_node* node){
     return node->left == NULL && node->right == NULL;
@@ -95,21 +97,96 @@ int onlyLeftChild(violation_node* node){
 int onlyRightChild(violation_node* node){
     return node->right != NULL && node->left == NULL;
 }
-violation_node* remove(violation_node* root, violation_node* node){
+violation_node* delete(violation_node* root, violation_node* delnode){
+    if(!root || !delnode) return root;
+    
+    violation_node *new_delnode, *save_node;
+    violation_node* p; // Parent of delnode
+    char save_name[MAX_NAME_LEN+1];
 
+    p = parent(root, delnode);
+    int cmp;
+    if(p != NULL)
+        cmp = strcmp(delnode->name, p->name);
+
+    if(isLeaf(delnode)){
+        if(p == NULL){
+            free(delnode);
+            return NULL;
+        }
+        else if(cmp < 0)
+            p->left = NULL;
+        else
+            p->right = NULL;
+        free(delnode);
+
+        return root;
+    }
+    else if(onlyLeftChild(delnode)){
+        if(p == NULL){
+            save_node = delnode->left;
+            free(delnode);
+            save_node->depth--;
+            return save_node;
+        }
+        else if(cmp < 0){
+            p->left = p->left->left;
+        }
+        else{
+            p->right = p->right->left;
+        }
+        free(delnode);
+        return root;
+    }
+    else if(onlyRightChild(delnode)){
+        if(p == NULL){
+            save_node = delnode->right;
+            free(delnode);
+            return save_node;
+        }
+        else if(cmp < 0){
+            p->left = p->left->right;
+        }
+        else{
+            p->right = p->right->right;
+        }
+        free(delnode);
+        return root;
+    }
+
+    new_delnode = maxVal(delnode->left);
+    // Update the values of the node that will replace the deleted node
+    strcpy(save_name, new_delnode->name);
+    delnode->fine_amt = new_delnode->fine_amt;
+
+    delete(root, new_delnode);
+
+    strcpy(delnode->name, save_name);
+
+    return root;
 }
+// Wrapper for delete that includes printf outputs and changes total final amount accordingly
 violation_node* deduct(violation_node* root, char name[MAX_NAME_LEN+1], int fineDeduct){
-    violation_node* temp = search(root, name);
-    if(temp == NULL) return NULL;
+    violation_node* temp = search(root, name, 0);
+    if(temp == NULL){
+        printf("%s not found\n", name);
+        return root;
+    }
 
-    if(temp->fine_amt < fineDeduct)
-        temp->fine_amt = 0;
-    else
+    // Delete node if deduction will result in 0 or negative fine
+    if(temp->fine_amt <= fineDeduct){
+        root = delete(root, temp);
+        printf("%s removed\n", name);
+        return root;
+    }
+    else{
         temp->fine_amt -= fineDeduct;
-
-    return temp;
+        printf("%s %d %d\n", name, temp->fine_amt, temp->depth);
+        return root;
+    }        
 }
 
+// Update sum and numNodes simultaneously while traveling through the tree
 void avgHelper(violation_node* root, int* sum, int* numNodes){
     if(root == NULL) return;
 
@@ -122,12 +199,15 @@ void avgHelper(violation_node* root, int* sum, int* numNodes){
 double average(violation_node* root){
     int sum = 0, numNodes = 0;
     avgHelper(root, &sum, &numNodes);
-    return (double)sum / numNodes;
+
+    // Avoid divide by zero
+    return (numNodes == 0) ? 0 : (double)sum / numNodes;
 }
 
 int findHeight(violation_node* root){
     if(root == NULL) return -1;
     
+    // Iterate through each node in left and right subtrees
     int lh = 1 + findHeight(root->left);
     int rh = 1 + findHeight(root->right);
 
@@ -136,6 +216,7 @@ int findHeight(violation_node* root){
 void heightBalance(violation_node* root){
     if(root == NULL) return;
     
+    // Find the heights of the left and right subtree
     int leftHeight = findHeight(root->left);
     int rightHeight = findHeight(root->right);
 
@@ -154,13 +235,24 @@ int calcBelow(violation_node* root, char name[MAX_NAME_LEN+1]){
     if(root == NULL) return 0;
 
     int cmp = strcmp(name, root->name);
-    if(cmp <= 0){
+    // Find first instance of node name being lexicographically smaller
+    // than given name 
+    if(cmp < 0){
         return calcBelow(root->left, name);
     }
+    // Node names less than current node will all be less than name, so sum them
+    // Find the next instance of node name being smaller than given name 
+    //  and repeat 
     return root->fine_amt + sum(root->left) + calcBelow(root->right, name);
 }
 
-void free_mem(violation_node* root){}
+void free_mem(violation_node* root){
+    if(root != NULL){
+        free_mem(root->left);
+        free_mem(root->right);
+        free(root);
+    }
+}
 
 int main(){
     int n;
@@ -169,62 +261,43 @@ int main(){
     violation_node* root = NULL;
 
     char com[COMMAND_MAX_LEN+1];
-    char commands[][COMMAND_MAX_LEN+1] = {"add", "deduct", "search", "average",
-                                          "height_balance", "calc_below"};
     while(n > 0){
         scanf("%s", com);
         char name[MAX_NAME_LEN+1];
-        int fine, depth;
-        if(strcmp(com, commands[0]) == 0){
+        int fine;
+        if(strcmp(com, "add") == 0){
             scanf("%s %d", name, &fine);
 
-            root = add(root, name, fine);
-            violation_node* temp = search(root, name);
-
-            printf("%s %d %d\n", name, temp->fine_amt, temp->depth);
+            root = add(root, name, fine, 0);
         }
-        else if(strcmp(com, commands[1]) == 0){
+        else if(strcmp(com, "deduct") == 0){
             scanf("%s %d", name, &fine);
 
-            violation_node* temp = deduct(root, name, fine);
-            if(temp != NULL){
-                printf("%s %d %d\n", name, temp->fine_amt, temp->depth);
-                if(temp->fine_amt == 0){
-                    root = remove(root, temp);
-                    printf("%s removed\n", name);
-                }
-            }
-            else
-                printf("%s not found\n", name);
+            root = deduct(root, name, fine); 
         }
-        else if(strcmp(com, commands[2]) == 0){
+        else if(strcmp(com, "search") == 0){
             scanf("%s", name);
 
-            violation_node* temp = search(root, name);
+            violation_node* temp = search(root, name, 0);
             if(temp != NULL){
                 printf("%s %d %d\n", name, temp->fine_amt, temp->depth);
             }
             else
                 printf("%s not found\n", name);
         }
-        else if(strcmp(com, commands[3]) == 0){
+        else if(strcmp(com, "average") == 0){
             printf("%.2lf\n", average(root));
         }
-        else if(strcmp(com, commands[4]) == 0){
+        else if(strcmp(com, "height_balance") == 0){
             heightBalance(root);
         }
-        else if(strcmp(com, commands[5]) == 0){
+        else if(strcmp(com, "calc_below") == 0){
             scanf("%s", name);
-            printf("name %d\n", calcBelow(root, name));
+            printf("%d\n", calcBelow(root, name));
         }
-        else{
-            fprintf(stderr, "Error: Invalid command");
-            break;
-        }
-        // print_inorder(root);
-        // puts("");
         n--;
     }
+    free_mem(root);
 
     return 0;
 }
