4348426fe91b1313e22374c875039d366136d6ff
joosnlaurence <jasongotanl@gmail.com>
2024-11-16 07:35:59 -0500

    Add hash tables lab

---
+++ lab-hashtables/hash-table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Table : Abstract data type that stores records according to search key values (sounds like a dictionary)
+// Each individual row in a table is its own record
+// Direct Access Table: Directly insert, access, or delete the values that you want by treating some
+// unique value as an index -> very bad for storage
+// Instead, we could try to map the unique IDs to some sort of number
+// Introducing, the hash table!
+// An array of table items, where the index is calculated by inputting the key into a hash function
+// A good hash function avoids collisions, is fast, and distributes items evenly (i.e. calculated indices don't 
+//  cluster around some group of values)
+// 
+
+
+int main(){
+    
+
+    return 0;
+}
